97cf4af78febf2d82b8964d51c98a020cf91df5e
unani <unani92@naver.com>
2020-12-25 00:19:02 +0900

    create components

---
+++ src/components/Main/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="btn">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Button",
+    props: {
+      name: String,
+      color: String
+    }
+  }
+</script>
+
+<style scoped>
+  .btn {
+
+  }
+</style>
